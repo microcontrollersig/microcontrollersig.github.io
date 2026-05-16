94c31844cab9ff13bafde7cc3b8365330f7ef662
Mohan Gupta <mohangupta@live.com>
2026-05-16 17:23:59 +1000

    initial comit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://microcontrollersig.github.io', 
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Inter",
+      cssVariable: "--font-inter", // Exposes it cleanly to Tailwind/CSS
+    },
+    {
+        provider: fontProviders.fontsource(),
+        name: "Newsreader",
+        cssVariable: "--font-newsreader", // Exposes it cleanly to Tailwind/CSS
+    },
+    ],
+    vite: {
+    plugins: [tailwindcss()],
+    }, 
+   
+});
