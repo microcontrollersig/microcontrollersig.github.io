51c14bc5b233b6c7f21f999790899112d55c9c51
Mohan Gupta <mohangupta@live.com>
2026-05-17 15:12:38 +1000

    slowly adding real content

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -8,13 +8,13 @@ const siteConfig: SiteConfig = {
         src: avatar,
         alt: 'Ethan Donovan'
     },
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: 'Microcontroller Workshop SIG',
+    subtitle: 'MelbPC User Group',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
-    image: {
-        src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
-    },
+    // image: {
+    //     src: '/dante-preview.jpg',
+    //     alt: 'Dante - Astro.js and Tailwind CSS theme'
+    // },
     headerNavLinks: [
         {
             text: 'Home',
@@ -66,8 +66,8 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence.\nMy approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products.\nI have a profound appreciation for top-notch software, visual design, and the principles of product-led growth.\n\nFeel free to explore some of my coding endeavors on [GitHub](https://github.com/JustGoodUI/dante-astro-theme) or follow me on [Twitter/X](https://twitter.com/justgoodui).",
+        title: '',
+        text: "Members helping members, interested in all things **Microcontrollers**, **ESP32**, **3D printing and modelling**,**PCB**, **CNC**, **Home Assistant**, **ESPHome** and the rest. Interested?",
         image: {
             src: hero,
             alt: 'A person sitting at a desk in front of a computer'
