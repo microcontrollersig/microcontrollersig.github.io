57ef2fd735d38e91c018b986b1f2ce6bb5404e40
Mohan Gupta <mohangupta@live.com>
2026-05-18 14:02:36 +1000

    minor content cleanup

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -45,10 +45,6 @@ const siteConfig: SiteConfig = {
         {
             text: 'Contact',
             href: '/contact'
-        },
-        {
-            text: 'Terms',
-            href: '/terms'
         }
     ],
     socialLinks: [
